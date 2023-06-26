471b6fb408b1f1b4d8468e9bbacb6cc4b58809c1
rikuriku0606 <yokoriku@gmail.com>
2023-06-26 07:58:30 +0000

    Add 初期設定基礎実装

--- config/app.php
+++ config/app.php
@@ -70,7 +70,7 @@ return [
     */
 
     'timezone' => 'Asia/Tokyo',
-
+    
     /*
     |--------------------------------------------------------------------------
     | Application Locale Configuration
