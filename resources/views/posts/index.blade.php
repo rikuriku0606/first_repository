45d0d08712a9794cd43cbf72040ebc96622ce9d2
rikuriku0606 <yokoriku@gmail.com>
2023-06-29 23:11:45 +0900

    カリキュラム08-3

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,20 +1,21 @@
 <!DOCTYPE html>
-<html lang="ja">
-  <head>
-    <meta charset="UTF-8" />
-    <title>blog</title>
-    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-  </head>
-  <body>
-      <h1>title</h1>
-      <div class='posts'>
-        @foreach ($posts as $post)
-        <div class='post'>
-          <h2 class='title'>{{ $post->title }}</h2>
-          <p class='body'>{{ $post->body }}</p>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <title>Blog</title>
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    </head>
+    <body class="antialiased">
+        <h1>Blog Name</h1>
+        <div class='posts'>
+          @foreach($posts as $post)
+            <div class='post'>
+                <a href="/posts/{{$posts->id}}"><h2 class='title'><a href="/posts/{{ $post->id }}">{{ $post->title }}</a></h2></a>
+                <p class='body'>{{$post->body}}</p>
+            </div>
+          @endforeach
         </div>
-        @endforeach
-      </div>
-      <dic class='paginate'>{{$posts->links()}}</dic>
-  </body>
+        <div class="paginate">{{$posts->links()}}</div>
+    </body>
 </html>
